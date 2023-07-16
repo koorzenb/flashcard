582a146878ecd0a5cd3d3d99dd9b39b3efab47b2
Barend Koorzen <koorzenb@gmail.com>
2023-07-16 20:33:18 +0200

    Added initial words

--- lib/models/word.dart
+++ lib/models/word.dart
@@ -11,22 +11,20 @@ class Word {
   Word({required this.hebrew, required this.pronunciation, required this.translation, this.attributes = ''});
 
   static Word getWord() {
-    final List<Word> words = [
-      //TODO: save to localStorage.
-      // save only on update/new
-      // Save to GoogleDrive
-      Word(hebrew: "אַבָּא", pronunciation: "a-bah", translation: "father", attributes: 'm, sl'),
-      Word(hebrew: "אִמָא", pronunciation: 'i-mah', translation: "mother", attributes: 'f, sl'),
-      Word(hebrew: "יוֹנָה", pronunciation: "joh-nah", translation: "dove"),
-      Word(hebrew: "לחם", pronunciation: "le-khem", translation: "bread"),
-    ];
-
+    final words = WordStorage.box.words;
     final randomIndex = Random().nextInt(words.length);
     return words[randomIndex];
   }
 
   static saveWord(Word word) {
-    final words = WordStorage.box.words;
+    // final words = WordStorage.box.words;
+    final List<Word> words = [
+      // TODO: Save to GoogleDrive
+      Word(hebrew: "אַבָּא", pronunciation: "a-bah", translation: "father", attributes: 'm, sl'),
+      Word(hebrew: "אִמָא", pronunciation: 'i-mah', translation: "mother", attributes: 'f, sl'),
+      Word(hebrew: "יוֹנָה", pronunciation: "joh-nah", translation: "dove"),
+      Word(hebrew: "לחם", pronunciation: "le-khem", translation: "bread"),
+    ];
     words.add(word);
     WordStorage.box.words = words;
   }
