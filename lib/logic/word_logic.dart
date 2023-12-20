01440f81bbb2f0bd01cff45092b40b8d4334fa77
Barend Koorzen <koorzenb@gmail.com>
2023-12-20 10:22:26 +0200

    Started on Firebase implementation

--- lib/logic/word_logic.dart
+++ lib/logic/word_logic.dart
@@ -4,20 +4,23 @@ import 'package:flashcard/models/word.dart';
 
 class WordLogic {
   List<Word> _allWords = [];
-  List<Word> _unDisplayedWords = [];
+  List<Word> _unReadWords = [];
 
   WordLogic(List<Word> words) {
     _allWords = words;
   }
 
   Word get word {
-    if (_unDisplayedWords.isEmpty) {
-      _unDisplayedWords = _allWords;
+    if (_unReadWords.isEmpty) {
+      _unReadWords = _allWords;
     }
     final Random random = Random();
-    final int randomIndex = random.nextInt(_unDisplayedWords.length);
-    final Word word = _unDisplayedWords[randomIndex];
-    _unDisplayedWords.removeAt(randomIndex);
+    final int randomIndex = random.nextInt(_unReadWords.length);
+    final Word word = _unReadWords[randomIndex];
+    _unReadWords.removeAt(randomIndex);
     return word;
+    //TODO: backup .env
+    implemtnt firebase - see Udemy lesson
+    https://firebase.google.com/docs/cli?hl=en&authuser=0&_gl=1*8yu11z*_ga*MTA0ODI4NTAyLjE3MDMwNTc5MzA.*_ga_CW55HF8NVT*MTcwMzA1NzkzMC4xLjEuMTcwMzA1ODQ2OC42MC4wLjA.#cli-ci-systems
   }
 }
