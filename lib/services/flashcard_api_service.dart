5a08d893549f7f6e1e4c47915e477583223a4489
Barend Koorzen <koorzenb@gmail.com>
2024-06-19 08:12:20 -0300

    (feat) Added _toJson factory

--- lib/services/flashcard_api_service.dart
+++ lib/services/flashcard_api_service.dart
@@ -2,10 +2,28 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flashcard/models/word.dart';
 
 class FlashCardApiService {
-  static void importWords() {
-    // fetch words from firebase
+  static void downloadWords() async {
+    final exportedWords = _toJson(await FirebaseFirestore.instance.collection('words').get());
 
-    // save to local storage
+    // add file to downloads
+    // show snackbar with success message
+    // add Drawer item
+  }
+
+  static List<Map<String, dynamic>> _toJson(QuerySnapshot querySnapshot) {
+    final List<Map<String, dynamic>> json = [];
+
+    querySnapshot.docs.forEach((doc) {
+      final word = {
+        'hebrew': doc['hebrew'],
+        'pronunciation': doc['pronunciation'],
+        'translation': doc['translation'],
+        'attributes': doc['attributes'],
+      };
+      json.add(word);
+    });
+
+    return json;
   }
 
   static void exportWords() {
